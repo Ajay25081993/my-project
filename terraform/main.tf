c16363288924255ac9370ee5bb10d0f661143bf5
unknown <ajaysarkar.bca@gmail.com>
2026-09-10 00:39:46 +0530

    Fix workflow step errors5

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0f684d40c1e7eee5c" # ✅ Valid in ap-south-2
   instance_type = "t2.micro"
 }
