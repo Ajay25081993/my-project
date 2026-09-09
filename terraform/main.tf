888345b5419728c53abb64eeeb37e96cdd2197d3
unknown <ajaysarkar.bca@gmail.com>
2026-09-10 00:51:12 +0530

    Fix workflow final

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0f684d40c1e7eee5c" # ✅ Valid in ap-south-2
-  instance_type = "t2.micro"
+  ami           = "ami-0f684d40c1e7eee5c" # ✅ Valid Ubuntu AMI in ap-south-2
+  instance_type = "t3.micro"              # ✅ Free Tier eligible in ap-south-2
 }
