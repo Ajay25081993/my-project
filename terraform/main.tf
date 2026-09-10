c20ec44c37d6c1ef96d297a7606deb7b6a1091f0
unknown <ajaysarkar.bca@gmail.com>
2026-09-10 14:37:06 +0530

    Update to flask

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,46 +2,35 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_security_group" "allow_flask" {
-  name        = "allow_flask_app"   # ✅ renamed to avoid duplicate
-  description = "Allow Flask app traffic"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Flask app access"
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Reference the existing security group by ID
+data "aws_security_group" "existing_flask" {
+  id = "sg-02097d70a68fda810"
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0f684d40c1e7eee5c"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_flask.id]
+  # Import this existing instance with terraform import
+  # terraform import aws_instance.my_ec2 i-03fe9dd428a1f3828
+  ami                    = "ami-0f684d40c1e7eee5c"   # keep for documentation, but Terraform will use the imported instance
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.existing_flask.id]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo apt-get update -y
-    sudo apt-get install -y docker.io awscli
-    sudo systemctl start docker
-    sudo systemctl enable docker
-
-    REGION="ap-south-2"
-    ACCOUNT_ID="206482633930"
-    REPO="my-app"
-    IMAGE_URI="$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO:latest"
-
-    aws ecr get-login-password --region $REGION | sudo docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
-    sudo docker pull $IMAGE_URI
-    sudo docker run -d -p 5000:5000 $IMAGE_URI
-  EOF
+  # user_data won't re-run on an imported instance unless you recreate it
+  # For existing EC2, you’ll need to SSH in and run Docker manually, or bake it into an AMI
+  # So leave user_data commented out here to avoid confusion
+  # user_data = <<-EOF
+  #   #!/bin/bash
+  #   sudo apt-get update -y
+  #   sudo apt-get install -y docker.io awscli
+  #   sudo systemctl start docker
+  #   sudo systemctl enable docker
+  #
+  #   REGION="ap-south-2"
+  #   ACCOUNT_ID="206482633930"
+  #   REPO="my-app"
+  #   IMAGE_URI="$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO:latest"
+  #
+  #   aws ecr get-login-password --region $REGION | sudo docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+  #   sudo docker pull $IMAGE_URI
+  #   sudo docker run -d --restart always -p 5000:5000 $IMAGE_URI
+  # EOF
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,9 @@
 output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
+  description = "Public IP of the existing EC2 instance"
   value       = aws_instance.my_ec2.public_ip
+}
+
+output "ec2_public_dns" {
+  description = "Public DNS of the existing EC2 instance"
+  value       = aws_instance.my_ec2.public_dns
 }
